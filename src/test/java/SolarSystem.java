5a366c4d46c5646b86d3740043e80d35ccb43677
ZionWhaley <dipsets159@gmail.com>
2024-02-17 19:48:20 -0500

    i have worked on planet1

--- src/test/java/SolarSystem.java
+++ src/test/java/SolarSystem.java
@@ -12,5 +12,12 @@ xros.color="Pink";
 xros.Temp = "213243";
 xros.Size = "2352353656miles";
 
+
+
+Planet1 frog = new Planet1();
+frog.color= "blue";
+frog.temp = "-1223132";
+frog.size = "2km";
+
     }
 }
